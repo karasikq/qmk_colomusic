e72ed5d9c99ea15dfaa17cd96eceed2a47db18dd
karasikq <romastepanuik@gmail.com>
2024-05-08 21:34:39 +0300

    VUMeter visualizer

--- src/visualizer.rs
+++ src/visualizer.rs
@@ -8,7 +8,6 @@
 */
 
 use std::fmt::Display;
-
 use ratatui::{buffer::Buffer, layout::*, style::Style, text::Line, widgets::Widget};
 
 #[derive(Copy, Clone)]
@@ -51,7 +50,7 @@ type Row = Vec<Key>;
 
 pub struct Layout {
     rows: [Row; 6],
-    pub colors: [u8; 84],
+    pub colors: [ratatui::style::Color; 84],
 }
 
 impl Default for Layout {
@@ -74,7 +73,7 @@ impl Default for Layout {
 
         Self {
             rows: [row_1, row_2, row_3, row_4, row_5, row_6],
-            colors: [0; 84],
+            colors: [ratatui::style::Color::Black; 84],
         }
     }
 }
@@ -102,18 +101,66 @@ impl<'a> Widget for LayoutWidget<'a> {
     {
         let mut x = 0;
         let mut absolute = 0;
-        let colors = &self.layout.colors;
-
         for (y, row) in self.layout.rows.iter().enumerate() {
             for key in row {
                 let key_str: &'static str = key.into();
-                let line = Line::from(key_str)
-                    .style(Style::default().fg(ratatui::style::Color::Rgb(colors[absolute], 0, 0)));
+                let color = self.layout.colors[absolute];
+                let line = Line::from(key_str).style(Style::default().fg(color));
                 buf.set_line(area.x + x, area.y + y as u16, &line, line.width() as u16);
                 x += line.width() as u16;
                 absolute += 1;
             }
             x = 0;
         }
+    }
+}
+
+pub struct VUMeterEmulator {
+    pub smooth: f32,
+    pub average_gain: f32,
+    pub average_attenuation: f32,
+    last_rms: (f32, f32),
+    average_level: f32,
+    max_level: f32,
+}
+
+impl VUMeterEmulator {
+    pub fn new(smooth: f32, average_gain: f32, average_attenuation: f32) -> Self {
+        Self {
+            smooth,
+            average_gain,
+            average_attenuation,
+            last_rms: (0f32, 0f32),
+            average_level: 0f32,
+            max_level: 0f32,
+        }
+    }
+
+    pub fn process(&mut self, rms: (f32, f32)) -> (f32, f32) {
+        self.last_rms.0 = rms.0 * self.smooth - self.last_rms.0 * (1.0f32 - self.smooth);
+        self.last_rms.1 = rms.1 * self.smooth - self.last_rms.1 * (1.0f32 - self.smooth);
+        self.average_level = (self.last_rms.0 + self.last_rms.1) / 2.0f32
+            * self.average_attenuation
+            + self.average_level * (1.0f32 - self.average_attenuation);
+        self.max_level = self.average_level * self.average_gain;
+        self.last_rms
+    }
+
+    pub fn max(&self) -> f32 {
+        self.max_level
+    }
+
+    pub fn average(&self) -> f32 {
+        self.average_level
+    }
+
+    pub fn map(x: f32, in_min: f32, in_max: f32, out_min: f32, out_max: f32) -> f32 {
+        (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+    }
+}
+
+impl Default for VUMeterEmulator {
+    fn default() -> Self {
+        Self::new(0.3f32, 1.6f32, 0.009f32)
     }
 }
